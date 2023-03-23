1a6c165f047e8d81e67e0fbe51a0bad794d0bc86
Pampam <piankovpe@gmail.com>
2023-03-23 23:24:54 +0600

    add user authentication

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -1,7 +1,12 @@
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta
 from typing import Optional
 from jose import jwt
+from jose.exceptions import JWTError
+from database.database import db_session
+from database.models import DbUsers
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -19,3 +24,25 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def get_current_user(token: str = Depends(oauth2_schema),
+                     db: Session = Depends(db_session),
+                     ):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail="Could not validate credentials",
+                                          headers={"WWW-Authenticate": "Bearer"},
+                                          )
+    try:
+        payload = jwt.decode(token=token, key=SECRET_KEY, algorithms=ALGORITHM)
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = db.query(DbUsers).filter(DbUsers.username == username).first()
+    if user is None:
+        raise credentials_exception
+    return user
+
