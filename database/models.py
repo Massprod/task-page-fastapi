01fefde95bf26e5fc0c082632239b6c549e4d5a9
Pampam <piankovpe@gmail.com>
2023-03-23 21:53:30 +0600

    fix primary/foreign keys

--- database/models.py
+++ database/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import relationship
 
 class DbTasks(Base):
     __tablename__ = "tasks"
-    id = Column(primary_key=True)
+    id = Column(Integer(), primary_key=True)
     name = Column(String(length=50), nullable=True)
     description = Column(String(length=300), nullable=False)
     user_id = Column(Integer(), ForeignKey("users.user_id"), nullable=True)
@@ -16,8 +16,8 @@ class DbTasks(Base):
 
 class DbUsers(Base):
     __tablename__ = "users"
-    user_id = Column(primary_key=True)
+    user_id = Column(Integer(), primary_key=True)
     username = Column(String(length=50), nullable=False)
     password = Column(String(length=50), nullable=False)
 
-    user_tasks = relationship("DbTasks", back_populates="user_id", cascade="all, delete, delete-orphan")
+    user_tasks = relationship("DbTasks", back_populates="user", cascade="all, delete, delete-orphan")
