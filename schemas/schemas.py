49b34208b95eb4dca14332d02bf0d1ea2d6a3bad
Pampam <piankovpe@gmail.com>
2023-03-23 21:54:10 +0600

    change schemas style

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,14 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class CreateNewUser(BaseModel):
-    username: str = "Marcus"
-    password: str = "Aurelius"
+    username: str = Field(default="Marcus",
+                          title="Preferred username",
+                          min_length=2,
+                          max_length=50,
+                          )
+    password: str = Field(default="Aurelius",
+                          title="Preferred password",
+                          min_length=8,
+                          max_length=50,
+                          )
 
 
 class CreateNewUserResponse(BaseModel):
-    user_id: int = 1
-    username: str = "Marcus"
+    user_id: int = Field(default=1,
+                         title="user id",
+                         )
+    username: str = Field(default="Marcus",
+                          title="Registered username",
+                          )
 
     class Config:
         orm_mode = True
