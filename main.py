c95589d693749994ca82c2062a43ecd9b8562c75
Pampam <piankovpe@gmail.com>
2023-03-24 16:16:38 +0600

    fix imports

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from database.database import Base, engine
-from routers.register import register_router
-from routers.task import task_router
+from routers.register_router import register_router
+from routers.task_router import task_router
 from auth.authentication import auth_router
 
 app = FastAPI(title="Tasks",
