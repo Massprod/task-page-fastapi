0f17d45a369f16df05ed5dc1062c1324de3fad77
Pampam <piankovpe@gmail.com>
2023-03-24 15:03:15 +0600

    add delete

--- database/crud/db_users.py
+++ database/crud/db_users.py
@@ -25,7 +25,7 @@ def get_all_users(db: Session) -> list[Type[DbUsers]]:
     return all_users
 
 
-def get_user(username: str, db: Session, user_id: int = None) -> Type[DbUsers] | None:
+def get_user(user_id: int | None, username: str | None, db: Session) -> Type[DbUsers] | None:
     """Find record in DbUsers with given username or user_id. Returns found record."""
     if user_id:
         user = db.query(DbUsers).filter_by(user_id=user_id).first()
@@ -35,23 +35,19 @@ def get_user(username: str, db: Session, user_id: int = None) -> Type[DbUsers] |
                                 )
         return user
     user = db.query(DbUsers).filter_by(username=username).first()
-    if username is None:
+    if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"There's no User with username: {username}",
                             )
     return user
 
 
-def update_user(username: str, db: Session, request: CreateNewUser, user_id: int = None) -> bool:
+def update_user(user_id: int | None, username: str | None, db: Session, request: CreateNewUser) -> bool:
     """Find record in DbUsers with given user_id or username. Update record with new data."""
     new_username = request.username
     new_hash_password = Hash().bcrypt_pass(request.password)
     if user_id:
-        user = db.query(DbUsers).filter_by(user_id=user_id).first()
-        if user is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail=f"There's no User with id: {user_id}",
-                                )
+        user = get_user(username=None, user_id=user_id, db=db)
         user.update(
             {DbUsers.username: new_username,
              DbUsers.password: new_hash_password,
@@ -59,15 +55,24 @@ def update_user(username: str, db: Session, request: CreateNewUser, user_id: int
         )
         db.commit()
         return True
-    user = db.query(DbUsers).filter_by(username=username).first()
-    if user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"There's no User with username: {username}",
-                            )
+    user = get_user(username=username, user_id=None, db=db)
     user.update(
         {DbUsers.username: new_username,
          DbUsers.password: new_hash_password,
          }
     )
+    db.commit()
+    return True
+
+
+def delete_user(user_id: int | None, username: str | None, db: Session) -> bool:
+    """Delete record from DbUsers with given user_id or username."""
+    if user_id:
+        user = get_user(username=None, user_id=user_id, db=db)
+        db.delete(user)
+        db.commit()
+        return True
+    user = get_user(username=username, user_id=None, db=db)
+    db.delete(user)
     db.commit()
     return True
