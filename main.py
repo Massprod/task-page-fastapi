8170a2f047f384c65677813134f3bbdcce79a0cc
Pampam <piankovpe@gmail.com>
2023-03-25 19:01:02 +0600

    add login router

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from database.database import Base, engine
 from routers.register_router import register_router
+from routers.login_router import login_router
 from routers.task_router import task_router
 from auth.authentication import auth_router
 
@@ -12,5 +13,7 @@ app = FastAPI(title="Tasks",
 app.include_router(auth_router)
 app.include_router(register_router)
 app.include_router(task_router)
+app.include_router(login_router)
+
 
 Base.metadata.create_all(engine)
