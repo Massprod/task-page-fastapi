c79ab066922a8ec7bc702c37ca6a27cfafc8ae29
Pampam <piankovpe@gmail.com>
2023-03-26 14:10:15 +0600

    delete login router

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from database.database import Base, engine
 from routers.register_router import register_router
-from routers.login_router import login_router
 from routers.task_router import task_router
 from auth.authentication import auth_router
 
@@ -13,7 +12,6 @@ app = FastAPI(title="Tasks",
 app.include_router(auth_router)
 app.include_router(register_router)
 app.include_router(task_router)
-app.include_router(login_router)
 
 
 Base.metadata.create_all(engine)
