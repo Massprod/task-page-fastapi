bb2a39dca7fca5fa643c4c30be2bedbce6dd8d19
Pampam <piankovpe@gmail.com>
2023-03-26 16:49:32 +0600

    add strip, change name

--- routers/register_func/functional.py
+++ routers/register_func/functional.py
@@ -4,12 +4,13 @@ from sqlalchemy.orm import Session
 from database.crud.db_users import create_user, get_user
 
 
-def create_new_user(user_data: CreateNewUser, db: Session) -> CreateNewUserResponse:
+def create_new_user(request: CreateNewUser, db: Session) -> CreateNewUserResponse:
     """Insert new Db record with User data"""
-    new_login = user_data.login
-    taken = get_user(login=new_login, user_id=None, db=db)
+    request.login = request.login.lower().strip(" ").replace(" ", "")
+    request.password = request.password.strip(" ").replace(" ", "")
+    taken = get_user(login=request.login, user_id=None, db=db)
     if taken:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail=f"Login: {taken.login} already taken")
-    new_user = create_user(db=db, request=user_data)
+    new_user = create_user(db=db, request=request)
     return new_user
