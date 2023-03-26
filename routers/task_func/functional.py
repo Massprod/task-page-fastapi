51f29d1eb081d21ca595f220a084147f48ddd53e
Pampam <piankovpe@gmail.com>
2023-03-26 20:57:47 +0600

    add get_one_task

--- routers/task_func/functional.py
+++ routers/task_func/functional.py
@@ -1,8 +1,9 @@
 from sqlalchemy.orm import Session
-from database.models import DbTasks
+from database.models import DbTasks, DbUsers
 from typing import Type
-from schemas.schemas import CreateNewTask, NewTaskResponse, ActiveUser, UpdateTask, AllTasksResponse, UpdateResponse
-from database.crud.db_tasks import create_task, update_task, get_all_tasks
+from schemas.schemas import CreateNewTask, NewTaskResponse, ActiveUser, UpdateTask
+from schemas.schemas import AllTasksResponse, UpdateResponse, OneTaskResponse
+from database.crud.db_tasks import create_task, update_task, get_all_tasks, get_task, validate_user_task
 from fastapi import HTTPException, status
 
 
@@ -13,16 +14,32 @@ def create_new_task(user: ActiveUser, db: Session, request: CreateNewTask) -> Ne
 
 
 def return_all_tasks(current_user: ActiveUser, db: Session) -> AllTasksResponse:
+    """Return all task records for Active user ID"""
     user_tasks = get_all_tasks(user=current_user, db=db)
     return AllTasksResponse(user_id=current_user.id,
                             user_tasks=user_tasks,
                             )
 
 
+def return_one_task(user: ActiveUser, task_id: int, db: Session) -> OneTaskResponse:
+    """Return task for given ID"""
+    task = get_task(user=user, task_id=task_id, db=db)
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Task with ID: {task_id} not found.")
+    return OneTaskResponse(task_id=task.task_id,
+                           name=task.name,
+                           description=task.description,
+                           status=task.status,
+                           )
+
+
 def update_existing_task(user: ActiveUser, request: UpdateTask, db: Session) -> UpdateResponse:
-    updated = update_task(db=db, request=request)
+    """Update and return updated Task info for """
+    user_id = user.id
+    updated = update_task(user=user, db=db, request=request)
     if updated:
-        return UpdateResponse(user_id=user.id,
+        return UpdateResponse(user_id=user_id,
                               updated=True,
                               name=updated.name,
                               description=updated.description,
--- routers/task_router.py
+++ routers/task_router.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Path
 from sqlalchemy.orm import Session
 from database.database import db_session
 from schemas.schemas import CreateNewTask, ActiveUser, NewTaskResponse
-from schemas.schemas import UpdateTask, UpdateResponse, AllTasksResponse
+from schemas.schemas import UpdateTask, UpdateResponse, AllTasksResponse, OneTaskResponse
 from auth.oauth2 import get_current_user
-from routers.task_func.functional import create_new_task, update_existing_task, return_all_tasks
+from routers.task_func.functional import create_new_task, update_existing_task, return_all_tasks, return_one_task
 
 task_router = APIRouter(prefix="/task",
                         tags=["task"],
@@ -24,8 +24,19 @@ async def add_new_task(request: CreateNewTask,
     return create_new_task(user=current_user, db=db, request=request)
 
 
+@task_router.get("/{task_id}",
+                 name="Get task",
+                 response_model=OneTaskResponse,
+                 )
+def get_one_task(task_id: int = Path(...),
+                 db: Session = Depends(db_session),
+                 current_user: ActiveUser = Depends(get_current_user),
+                 ):
+    return return_one_task(user=current_user, task_id=task_id, db=db)
+
+
 @task_router.get("/all",
-                 name="all tasks",
+                 name="Get all tasks",
                  response_model=AllTasksResponse,
                  description="Returns all tasks for currently Active user",
                  response_description="Successful response with all Task for user ID"
@@ -36,12 +47,12 @@ async def get_all_tasks(current_user: ActiveUser = Depends(get_current_user),
     return return_all_tasks(current_user=current_user, db=db)
 
 
-@task_router.post("/update",
-                  name="update task",
-                  response_model=UpdateResponse,
-                  description="Updating already existing task with new Data",
-                  response_description="Successful response with Updated task data"
-                  )
+@task_router.put("/update",
+                 name="update task",
+                 response_model=UpdateResponse,
+                 description="Updating already existing task with new Data",
+                 response_description="Successful response with Updated task data"
+                 )
 async def update_task_by_id(request: UpdateTask,
                             current_user: ActiveUser = Depends(get_current_user),
                             db: Session = Depends(db_session),
