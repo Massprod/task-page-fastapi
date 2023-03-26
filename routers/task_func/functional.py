ed7dde5285c79c0c5ad2d9b0edd3a0b0113136dd
Pampam <piankovpe@gmail.com>
2023-03-26 18:46:07 +0600

    add update

--- routers/task_func/functional.py
+++ routers/task_func/functional.py
@@ -1,9 +1,20 @@
 from sqlalchemy.orm import Session
-from schemas.schemas import CreateNewTask, ResponseNewTask, ActiveUser
-from database.crud.db_tasks import create_task
+from database.models import DbUsers
+from schemas.schemas import CreateNewTask, ResponseNewTask, ActiveUser, UpdateTask, UpdateResponse
+from database.crud.db_tasks import create_task, update_task
+from fastapi import HTTPException, status
 
 
 def create_new_task(user: ActiveUser, db: Session, request: CreateNewTask) -> ResponseNewTask:
     """Create a new Task record."""
     new_task = create_task(user=user, db=db, request=request)
     return new_task
+
+
+def update_exist_task(request: UpdateTask, db: Session):
+    updated = update_task(db=db, request=request)
+    if updated:
+        return updated
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Task with ID: {request.task_id} not found.",
+                        )
