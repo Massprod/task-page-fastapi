f378ff1ec3a05c1b506a9d98da5658de946515ae
Pampam <piankovpe@gmail.com>
2023-03-26 22:00:36 +0600

    change description

--- routers/task_router.py
+++ routers/task_router.py
@@ -15,7 +15,7 @@ task_router = APIRouter(prefix="/task",
 @task_router.post("/new",
                   name="new task",
                   response_model=NewTaskResponse,
-                  description="Create new task with given Name, Description",
+                  description="Create new task with given Name, Description for currently Active user",
                   response_description="Successful response with user Id and created Task data",
                   )
 async def add_new_task(request: CreateNewTask,
@@ -39,7 +39,7 @@ async def get_all_tasks(current_user: ActiveUser = Depends(get_current_user),
 
 @task_router.delete("/all",
                     name="Delete all tasks",
-                    description="Deletes all Tasks created by active User",
+                    description="Deletes all Tasks created by currently Active user",
                     )
 async def delete_all(current_user: ActiveUser = Depends(get_current_user),
                      db: Session = Depends(db_session),
@@ -65,7 +65,8 @@ def get_one_task(task_id: int = Path(title="Task Id",
 @task_router.put("/{task_id}",
                  name="update task",
                  response_model=UpdateResponse,
-                 description="Updating already existing task with new Data",
+                 description="Updating already existing task with new Data, "
+                             "if it's created by currently Active user",
                  response_description="Successful response with Updated task data"
                  )
 async def update_task_by_id(request: UpdateTask,
@@ -80,7 +81,7 @@ async def update_task_by_id(request: UpdateTask,
 
 @task_router.delete("/{task_id}",
                     name="delete task",
-                    description="Deleting record with chosen ID if it's created by Active user",
+                    description="Deleting record with chosen ID, if it's created by Active user",
                     )
 async def delete_task_by_id(task_id: int = Path(title="Task Id",
                                                 description="ID of existing task created by Active user",
