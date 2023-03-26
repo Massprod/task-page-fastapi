58ffb04f1a3534675943325b1530de60b67193d0
Pampam <piankovpe@gmail.com>
2023-03-26 18:46:13 +0600

    add update

--- routers/task_router.py
+++ routers/task_router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database.database import db_session
-from schemas.schemas import CreateNewTask, ActiveUser, ResponseNewTask
+from schemas.schemas import CreateNewTask, ActiveUser, ResponseNewTask, UpdateTask, UpdateResponse
 from auth.oauth2 import get_current_user
-from routers.task_func.functional import create_new_task
+from routers.task_func.functional import create_new_task, update_exist_task
 
 task_router = APIRouter(prefix="/task",
                         tags=["task"],
@@ -21,3 +21,14 @@ async def add_new_task(request: CreateNewTask,
                        db: Session = Depends(db_session),
                        ):
     return create_new_task(user=current_user, db=db, request=request)
+
+
+@task_router.post("/update",
+                  name="update task",
+                  response_model=UpdateResponse,
+                  )
+async def update_task_by_id(request: UpdateTask,
+                            current_user: ActiveUser = Depends(get_current_user),
+                            db: Session = Depends(db_session),
+                            ):
+    return update_exist_task(request=request, db=db)
