e3208ffea379fb74e7a0c648bfb066e50aba9275
Pampam <piankovpe@gmail.com>
2023-03-27 23:50:21 +0600

    add get_user/get_all

--- routers/user_func/functional.py
+++ routers/user_func/functional.py
@@ -4,10 +4,10 @@ from typing import Type
 from fastapi.responses import HTMLResponse
 from schemas.user_schemas import CreateNewUser, CreateNewUserResponse, UpdateUser, ActiveUser
 from sqlalchemy.orm import Session
-from database.crud.db_users import create_user, get_user, validate_user_id, update_user, delete_user
+from database.crud.db_users import create_user, get_user, validate_user_id, update_user, delete_user, get_all_users
 
 
-def create_new_user(request: CreateNewUser, db: Session) -> CreateNewUserResponse:
+def create_new_user(request: CreateNewUser, db: Session) -> DbUsers:
     """Insert new Db record with User data"""
     request.login = request.login.lower().strip(" ").replace(" ", "")
     request.password = request.password.strip(" ").replace(" ", "")
@@ -43,3 +43,23 @@ def delete_user_credentials(user_id: int, current_user: ActiveUser, db: Session)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                         detail=f"UserID: {user_id} doesn't exist",
                         )
+
+
+# No reasons to use it in this practice. But if there was more data about Users to display, then it could be useful.
+def get_user_data(user_id: int, current_user: ActiveUser, db: Session) -> Type[DbUsers]:
+    """Getting everything about user, except password"""
+    validate_user_id(user_id=user_id, current_user=current_user)
+    user = get_user(user_id=user_id, login=None, db=db)
+    if user:
+        return user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"UserID: {user_id} doesn't exist",
+                        )
+
+
+def get_all(current_user: ActiveUser, db: Session) -> list[Type[DbUsers]]:
+    """Getting list of all users data"""
+    validate_user_id(current_user=current_user, user_id=None)
+    users = get_all_users(db=db)
+    return users
+
