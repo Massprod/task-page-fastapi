c720c60961f0c700ea04d2f4b08425d04ec40cde
Pampam <piankovpe@gmail.com>
2023-03-27 20:15:27 +0600

    add delete user route

--- routers/user_func/functional.py
+++ routers/user_func/functional.py
@@ -1,8 +1,10 @@
 from fastapi import HTTPException, status
+from database.models import DbUsers
+from typing import Type
 from fastapi.responses import HTMLResponse
 from schemas.user_schemas import CreateNewUser, CreateNewUserResponse, UpdateUser, ActiveUser
 from sqlalchemy.orm import Session
-from database.crud.db_users import create_user, get_user, validate_user_id, update_user
+from database.crud.db_users import create_user, get_user, validate_user_id, update_user, delete_user
 
 
 def create_new_user(request: CreateNewUser, db: Session) -> CreateNewUserResponse:
@@ -12,7 +14,8 @@ def create_new_user(request: CreateNewUser, db: Session) -> CreateNewUserRespons
     taken = get_user(login=request.login, user_id=None, db=db)
     if taken:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail=f"Login: {taken.login} already taken")
+                            detail=f"Login: {taken.login} already taken",
+                            )
     new_user = create_user(db=db, request=request)
     return new_user
 
@@ -20,7 +23,23 @@ def create_new_user(request: CreateNewUser, db: Session) -> CreateNewUserRespons
 def update_user_credentials(request: UpdateUser,
                             user_id: int,
                             current_user: ActiveUser,
-                            db: Session) -> HTMLResponse:
+                            db: Session) -> Type[DbUsers]:
+    """Update existing Db record for Active user with new User data"""
     validate_user_id(current_user=current_user, user_id=user_id)
-    update_user(user_id=user_id, db=db, request=request)
-    return HTMLResponse(status_code=status.HTTP_200_OK)
+    updated = update_user(user_id=user_id, db=db, request=request)
+    if updated:
+        return updated
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"UserID: {user_id} doesn't exist",
+                        )
+
+
+def delete_user_credentials(user_id: int, current_user: ActiveUser, db: Session) -> HTMLResponse:
+    """Delete existing Db record for Active user"""
+    validate_user_id(current_user=current_user, user_id=user_id)
+    deleted = delete_user(user_id=user_id, db=db)
+    if deleted:
+        return HTMLResponse(status_code=status.HTTP_204_NO_CONTENT)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"UserID: {user_id} doesn't exist",
+                        )
--- routers/user_router.py
+++ routers/user_router.py
@@ -2,30 +2,31 @@ from fastapi import APIRouter, Depends, Path
 from auth.oauth2 import get_current_user
 from sqlalchemy.orm.session import Session
 from database.database import db_session
-from schemas.user_schemas import CreateNewUser, CreateNewUserResponse, ActiveUser, UpdateUser
-from routers.user_func.functional import create_new_user, update_user_credentials
+from schemas.user_schemas import CreateNewUser, CreateNewUserResponse, ActiveUser, UpdateUser, UpdateUserResponse
+from routers.user_func.functional import create_new_user, update_user_credentials, delete_user_credentials
+
+user_router = APIRouter(prefix="/user",
+                        tags=["user"],
+                        )
 
 
-register_router = APIRouter(prefix="/user",
-                            tags=["user"],
-                            )
-
-
-@register_router.post("/new",
-                      name="New User",
-                      response_model=CreateNewUserResponse,
-                      description="Register new User with given credentials",
-                      response_description="Successful response with Id and Login of registered User",
-                      )
+@user_router.post("/new",
+                  name="New User",
+                  response_model=CreateNewUserResponse,
+                  description="Register new User with given credentials",
+                  response_description="Successful response with Id and Login of registered User",
+                  )
 async def register_new_user(request: CreateNewUser,
                             db: Session = Depends(db_session),
                             ):
     return create_new_user(request, db)
 
 
-@register_router.put("{user_id}",
-                     name="Update user",
-                     )
+@user_router.put("{user_id}",
+                 name="Update user",
+                 response_model=UpdateUserResponse,
+                 response_description="Successful response with Id and New login of updated User"
+                 )
 async def update_exist_user(request: UpdateUser,
                             user_id: int = Path(title="User Id",
                                                 description="Id of registered User"),
@@ -33,3 +34,16 @@ async def update_exist_user(request: UpdateUser,
                             db: Session = Depends(db_session)
                             ):
     return update_user_credentials(request=request, user_id=user_id, current_user=current_user, db=db)
+
+
+@user_router.delete("{user_id}",
+                    name="Delete User",
+                    description="Delete user data from Db with given UserId as identifier",
+                    )
+async def delete_exist_user(user_id: int = Path(title="User Id",
+                                                description="Id of registered User",
+                                                ),
+                            current_user: ActiveUser = Depends(get_current_user),
+                            db: Session = Depends(db_session),
+                            ):
+    return delete_user_credentials(user_id=user_id, current_user=current_user, db=db)
