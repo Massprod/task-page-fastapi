a8816c151749ca4c7cbea5853392e626d3129d2b
Pampam <piankovpe@gmail.com>
2023-04-01 00:38:32 +0600

    add admin

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,14 @@
 from tests.user_fixtures import *
+from conf_test_db import override_db_session
+from database.models import DbUsers
+from database.hash import Hash
+
+
+def pytest_sessionstart():
+    sess = next(override_db_session())
+    sess.add(DbUsers(id=1,
+                     login="admin",
+                     password=Hash().bcrypt_pass("admin"),
+                     )
+             )
+    sess.commit()
