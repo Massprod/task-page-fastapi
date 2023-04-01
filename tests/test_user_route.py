6329c0de9996e11bc6d855bfaaa55e43bf6174bd
Pampam <piankovpe@gmail.com>
2023-04-01 16:41:47 +0600

    add get user/all tests

--- tests/test_user_route.py
+++ tests/test_user_route.py
@@ -125,6 +125,7 @@ async def test_delete_user_with_correct_token(test_client,
                                               database,
                                               access_token,
                                               ):
+    """Test delete existing user data with correct access-token for given id"""
     test_token = await access_token
     test_id = get_current_user(token=test_token, db=database).id
     response = await test_client.delete(f"user/{test_id}",
@@ -141,6 +142,7 @@ async def test_delete_user_with_admin_access(test_client,
                                              admin_token,
                                              credentials,
                                              ):
+    """Test deleting with admin access-token"""
     test_login = credentials["login"]
     test_password = credentials["password"]
     test_admin_token = await admin_token
@@ -168,3 +170,95 @@ async def test_delete_user_with_admin_access(test_client,
                                                      }
                                             )
     assert admin_delete.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_get_user_with_correct_token(access_token,
+                                           database,
+                                           test_client,
+                                           ):
+    """Test getting user data with correct access-token"""
+    test_token = await access_token
+    test_id = get_current_user(token=test_token, db=database).id
+    response = await test_client.get(f"user/{test_id}",
+                                     headers={"Authorization": f"Bearer {test_token}",
+                                              }
+                                     )
+    assert response.status_code == 200
+    exist = database.query(DbUsers).filter_by(id=test_id).first()
+    assert exist
+
+
+@pytest.mark.asyncio
+async def test_get_user_with_incorrect_token(access_token,
+                                             database,
+                                             test_client,
+                                             ):
+    """Test getting other users data with correct access-token"""
+    test_token = await access_token
+    test_id = 10002
+    response = await test_client.get(f"user/{test_id}",
+                                     headers={"Authorization": f"Bearer {test_token}",
+                                              }
+                                     )
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_get_user_with_admin_token(admin_token,
+                                         database,
+                                         test_client,
+                                         access_token,
+                                         ):
+    """Test getting any user data with admin access-token"""
+    test_token = await access_token
+    test_id = get_current_user(token=test_token, db=database).id
+    test_admin_token = await admin_token
+    not_exist_id = 10002
+    response = await test_client.get(f"user/{test_id}",
+                                     headers={"Authorization": f"Bearer {test_admin_token}",
+                                              }
+                                     )
+    assert response.status_code == 200
+    response_data = response.json()
+    assert response_data["id"] == test_id
+    exist = database.query(DbUsers).filter_by(id=response_data["id"]).first()
+    assert exist
+    admin_call = await test_client.get(f"user/1",
+                                       headers={"Authorization": f"Bearer {test_admin_token}",
+                                                }
+                                       )
+    assert admin_call.status_code == 200
+    not_exist_id_call = await test_client.get(f"user/{not_exist_id}",
+                                              headers={"Authorization": f"Bearer {test_admin_token}",
+                                                       }
+                                              )
+    assert not_exist_id_call.status_code == 404
+    not_exist = database.query(DbUsers).filter_by(id=not_exist_id).first()
+    assert not_exist is None
+
+
+@pytest.mark.asyncio
+async def test_get_all_users_with_admin_token(admin_token,
+                                              test_client,
+                                              ):
+    """Test getting all users data with admin access-token"""
+    test_admin_token = await admin_token
+    response = await test_client.get(f"user/all",
+                                     headers={"Authorization": f"Bearer {test_admin_token}",
+                                              }
+                                     )
+    assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_get_all_users_with_incorrect_access_token(access_token,
+                                                         test_client,
+                                                         ):
+    """Test getting all users data with any not admin access-token"""
+    test_token = await access_token
+    response = await test_client.get(f"user/all",
+                                     headers={"Authorization": f"Bearer {test_token}",
+                                              }
+                                     )
+    assert response.status_code == 403
